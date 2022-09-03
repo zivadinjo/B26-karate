50fb22fffa6510f6744fb98a911c4d4946adbb37
zivadinjo <zivadinjo1985@gmail.com>
2022-09-03 11:18:06 -0500

    Karate

--- src/test/java/com/cydeo/examples/ExamplesTest.java
+++ src/test/java/com/cydeo/examples/ExamplesTest.java
@@ -5,7 +5,8 @@ import com.intuit.karate.junit5.Karate;
 class ExamplesTest {
     
     // this will run all *.feature files that exist in sub-directories
-    // see https://github.com/intuit/karate#naming-conventions   
+    // see https://github.com/intuit/karate#naming-conventions
+//
     @Karate.Test
     Karate testAll() {
         return new Karate().relativeTo(getClass());
